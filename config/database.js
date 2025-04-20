71b2af6074c9d10823ba774c1247943295f65d2b
Indo86 <deninorman77@gmail.com>
2025-04-20 18:56:23 +0700

    update: config and cloudbuild

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("scribly_note", "root", "", {
-  host: "localhost",
+const db = new Sequelize("scribly_note", "root", "password123", {
+  host: "34.56.154.227",
   dialect: "mysql"
 })
 
